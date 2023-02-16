637c317bf166f1af71e824ff2268387b22fc261f
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-16 18:17:46 -0300

    Today I learned about privates attributes, code encapsulation, encapsulation of an attribute manipulation in methods and code cohesion

--- conta.py
+++ conta.py
@@ -2,15 +2,19 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo Objeto {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     def extrato(self):
-        print("Saldo de R${} do pobre titular {}".format(self.saldo, self.titular))
+        print("Saldo de R${} do pobre titular {}".format(self.__saldo, self.__titular))
     def deposito(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     def saque(self, saque):
-        self.saldo -= saque
-        print("Seu saldo restante é {}".format(self.saldo))
+        self.__saldo -= saque
+        print("Seu saldo restante é {}".format(self.__saldo))
+
+    def transferencia(self, valor, destino):
+        self.saque(valor)
+        destino.desposita(valor)
 
