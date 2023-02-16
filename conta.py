574a7c9a50bc980b7eacb1798de4e544fb3fc01d
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-16 19:11:31 -0300

    Attribute reading methods using getters, Attribute modification methods using the setters and properties

--- conta.py
+++ conta.py
@@ -18,3 +18,18 @@ class Conta:
         self.saque(valor)
         destino.desposita(valor)
 
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
