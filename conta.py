77472b11f2daec319df4b463571e25fb2d506da5
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-16 21:49:59 -0300

    Private Methods, Class methods and static methods

--- conta.py
+++ conta.py
@@ -6,13 +6,23 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+
     def extrato(self):
         print("Saldo de R${} do pobre titular {}".format(self.__saldo, self.__titular))
     def deposito(self, valor):
         self.__saldo += valor
-    def saque(self, saque):
-        self.__saldo -= saque
-        print("Seu saldo restante é {}".format(self.__saldo))
+        print("Seu novo saldo agora é de {}".format(self.__saldo))
+
+    def __able_sacar(self, valor_a_sacar):
+        valor_disponivel_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_sacar
+
+    def saque(self, valor):
+        if(self.__able_sacar(valor)):
+            self.__saldo -= valor
+            print("Seu saldo restante é {}".format(self.__saldo))
+        else:
+            print("Voce nao tem esse dinhiero de limite, compra nao realizada ;-;")
 
     def transferencia(self, valor, destino):
         self.saque(valor)
@@ -33,3 +43,7 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
